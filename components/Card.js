9e43ccf713a5e110e55fffc7acde0114a914cdcd
Camilo Jimenez <camilojj14@gmail.com>
2022-09-15 19:07:30 -0500

    feat: add moment js

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,19 @@
 import { deleteUser } from '../services/users'
 import UpdateForm from './UpdateForm'
+import moment from 'moment'
 
 export default function Card({ user, setUsers }) {
   const handleDelete = async () => {
     const res = await deleteUser(user.id)
     setUsers(prevUsers => prevUsers.filter(prevUser => prevUser.id !== user.id))
   }
+
+
   return (
     <div style={{padding: '20px'}}>
       <div>{ user.name }</div>
-      <div>{ user.birth }</div>
-      <div>{user.nit}</div>
+      <div>{ moment(user.birth).format('Do MMM YYYY') }</div>
+      <div>{ user.nit }</div>
       <button onClick={handleDelete}>Delete</button>
       <button>Update</button>
       <UpdateForm user={user} setUsers={setUsers} />
